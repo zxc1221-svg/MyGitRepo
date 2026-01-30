8042fe69eceb136ac8270e15a3c4433c30de3b4e
ST-008\user <e@gmail.com>
2026-01-30 11:22:31 +0800

    存檔

--- hello.py
+++ hello.py
@@ -1,2 +1,21 @@
-a=2
-print(a)
+k=60
+print(k)
+print(id(k))
+x=30
+print(x)
+print(id(x))
+y=x
+print("y="+str(y))
+print("id(y)="+str(id(y)))
+result=x+y
+print("result="+str(result))
+print("id(result)="+str(id(result)))
+'''
+Immutable objects example
+不可變物件範例
+int str float tuple
+
+Mutable objects example
+可變物件範例
+list dict set
+'''
