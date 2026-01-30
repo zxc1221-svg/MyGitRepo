fabc987564e45dd0d02715877e35c754c32ea738
ST-008\user <e@gmail.com>
2026-01-30 11:54:12 +0800

    程式練習

---
+++ InputOutput.py
@@ -0,0 +1,19 @@
+#如何進行輸入和輸出操作
+#This program demonstrates basic input and output operations in Python.
+#給我輸入和輸出的範例程式碼
+
+var1="第一個變數"
+print("var1="+str(var1))
+print("=========")
+print("var1=",str(var1),sep="@")
+'''
+print("Hello World!", end="")
+print("Hello World!")
+print("var1 = ", var1, end="")
+print("var1 = ", var1)
+print("var1 = " + str(var1), end="")
+print("var1 = " + str(var1))
+print(var1, end="")
+print(var1)
+
+'''
---
+++ Logic.py
@@ -0,0 +1,62 @@
+'''var1=15
+if var1 >= 10:
+    print("condition is true")
+    print("條件為真")
+print("結束條件判斷")
+
+'''
+'''
+var1=9
+
+if var1 >= 10:
+    print("condition is true")
+    print("條件為真")
+else:
+    print("condition is false")
+print("結束條件判斷")
+'''
+
+
+
+'''
+var1=9
+
+if var1 >= 20:
+    print("this condition is true")
+elif var1 >= 10:
+    print("this condition is true")
+else:
+    print("conditions are false")
+print("結束條件判斷")   
+
+
+'''
+
+
+
+
+for i in range(1, 11):
+    print("i = " + str(i))
+
+
+
+
+'''i = 1
+while i <= 10:
+    print("i = " + str(i))
+    i = i + 1
+'''
+
+
+
+'''if i > 5:
+    break
+'''
+
+
+
+'''if i % 2 == 0:
+    continue
+'''
+
+ 
