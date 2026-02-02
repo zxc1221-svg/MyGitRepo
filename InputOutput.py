f113a8a266e41dfafdb87cb6afc685a44826e739
ST-008\user <e@gmail.com>
2026-02-02 11:57:53 +0800

    Practice

--- InputOutput.py
+++ InputOutput.py
@@ -2,10 +2,11 @@
 #This program demonstrates basic input and output operations in Python.
 #給我輸入和輸出的範例程式碼
 
-var1="第一個變數"
+'''var1="第一個變數"
 print("var1="+str(var1))
 print("=========")
 print("var1=",str(var1),sep="@")
+'''
 '''
 print("Hello World!", end="")
 print("Hello World!")
@@ -14,6 +15,12 @@ print("var1 = ", var1)
 print("var1 = " + str(var1), end="")
 print("var1 = " + str(var1))
 print(var1, end="")
-print(var1)
+print(var1)'''
 
+var1 = int(input("身高整數值 =>"))
+print("You entered integer:", var1)
+
+'''
+var1 = float(input("Please enter a floating point number =>"))
+var1 = input("Please enter a string =>")
 '''
--- Logic.py
+++ Logic.py
@@ -35,28 +35,53 @@ print("結束條件判斷")
 
 
 
-for i in range(1, 11):
+'''
+for i in range(11):
     print("i = " + str(i))
+'''
 
-
-
-
-'''i = 1
+'''
+i=1
 while i <= 10:
     print("i = " + str(i))
-    i = i + 1
+    i = i+1
 '''
 
 
 
-'''if i > 5:
+
+'''
+if i > 5:
     break
-'''
 
 
-
-'''if i % 2 == 0:
+if i % 2 == 0:
     continue
 '''
+
+
+def BMI(para1, para2)->float:
+    height=para1/100
+    weight=para2
+    bmi=weight/(height*height)
+    return bmi
+ 
+height=float(input("請輸入身高(公分):"))
+weight=float(input("請輸入體重(公斤):"))
+bmi=BMI(height,weight)
+print("BMI=" + str(bmi))
+
+    
+if bmi<18.5:
+    print("體重過輕")
+elif bmi>=18.5 and bmi<24:
+    print("體重正常")
+elif bmi>=24 and bmi<27:
+    print("體重過重")
+else:
+    print("體重肥胖")
+
+
+
 
  
